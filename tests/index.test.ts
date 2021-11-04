5272e4de0348661f1f6a413d3df934a45d1c0656
Wasiu Idowu <hoxtygen@live.com>
2021-11-04 23:03:57 +0100

    Fixed failing test

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -12,12 +12,11 @@ describe('Testing / route', () => {
         })
     });
 
-    it("should return welcome message for /  api version 1 route", (done) => {
-        request(app).get("/api/v1").expect(200).end((err, res) => {
-            if (err) return done(err)
-            expect(res.body).toMatchObject({ message: "Welcome to Politico API v1. Documentation coming soon." })
-            done()
-        })
+    it("should return status code for  /  api version 1 route", async () => {
+        const res = await request(app).get("/api-docs.json");
+        expect(res.status).toBe(200);
+        expect(res.type).toBe("application/json")
+
     });
 
 })
